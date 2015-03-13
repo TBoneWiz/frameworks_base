e13d18ed74a582f2b304b06bdcbe5073fc584127
Preeti Ahuja <preetia@codeaurora.org>
2015-03-13 16:00:58 -0700

    Handle NPE in TelephonyManager
    ITelphony service instance could be null at boot up
    when device activation is taking place.
    
    An additional NPE is handled here compared to this commit's original
    form (If72622a2d22a20b1ce909134ce4c6446b132dffe):
      NPE exception handling in getDataEnabled has been backported from:
      GpsLocationProvider: Read data enabled status from TelephonyManager.
      I0333050d2a5b8d484466e1b1914dcfb9965398c9
    
      This addresses BUGDUMP-1125891

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -4254,7 +4254,9 @@ public class TelephonyManager {
             if (telephony != null)
                 return telephony.needsOtaServiceProvisioning();
         } catch (RemoteException e) {
-            Log.e(TAG, "Error calling ITelephony#needsOtaServiceProvisioning", e);
+            Log.e(TAG, "Error calling ITelephony#isDataPossibleForSubscription", e);
+        } catch (NullPointerException npe) {
+            Log.e(TAG, "Error calling ITelephony#isDataPossibleForSubscription", npe);
         }
         return false;
     }
@@ -4282,7 +4284,9 @@ public class TelephonyManager {
             if (telephony != null)
                 telephony.setDataEnabled(subId, enable);
         } catch (RemoteException e) {
-            Log.e(TAG, "Error calling setDataEnabled", e);
+            Log.e(TAG, "Error calling ITelephony#needsOtaServiceProvisioning", e);
+        } catch (NullPointerException npe) {
+            Log.e(TAG, "Error calling ITelephony#needsOtaServiceProvisioning", npe);
         }
     }
 
@@ -4349,7 +4353,8 @@ public class TelephonyManager {
             if (telephony != null)
                 return telephony.isVideoCallingEnabled(getOpPackageName());
         } catch (RemoteException e) {
-            Log.e(TAG, "Error calling ITelephony#isVideoCallingEnabled", e);
+            Log.e(TAG, "Error calling ITelephony#getDataEnabled", e);
+        } catch (NullPointerException e) {
         }
         return false;
     }
