14a6ec16f34d88943652235b562606bd8392ed6e
Winson Chung <winsonc@google.com>
2015-12-16 13:18:43 -0500

    BACKPORT: Adding catch for b/26221779
    Change-Id: Id0cf455cd65bf5b202a6249b0b51351addb0bd17

--- packages/SystemUI/src/com/android/systemui/recents/model/RecentsTaskLoader.java
+++ packages/SystemUI/src/com/android/systemui/recents/model/RecentsTaskLoader.java
@@ -239,9 +239,16 @@ class TaskResourceLoader implements Runnable {
 
     Drawable getTaskDescriptionIcon(Task.TaskKey taskKey, Bitmap iconBitmap, String iconFilename,
             SystemServicesProxy ssp, Resources res) {
-        Bitmap tdIcon = iconBitmap != null
-                ? iconBitmap
-                : ActivityManager.TaskDescription.loadTaskDescriptionIcon(iconFilename);
+        Bitmap tdIcon = null;
+        if (iconBitmap != null) {
+            tdIcon = iconBitmap;
+        } else {
+            try {
+                tdIcon = ActivityManager.TaskDescription.loadTaskDescriptionIcon(iconFilename);
+            } catch (Exception e) {
+                // TODO: Investigate for b/26221779
+            }
+        }
         if (tdIcon != null) {
             return ssp.getBadgedIcon(new BitmapDrawable(res, tdIcon), taskKey.userId);
         }
