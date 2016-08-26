f30ac743c796644b068a87bda451e8349fe734d0
TboneWiz <netfluke@yahoo.com>
2016-08-25 22:01:13 -0400

    Fix FC when holding home button down
    cryptickid committed with NL-BlackDragon on Nov 9, 2015
    
    Credits to Cyanidel
    https://github.com/CyanideL/android_frameworks_base/commit/7a49a6937996b9d58368144ebae93acca298d309

--- packages/SystemUI/src/com/android/systemui/assist/AssistManager.java
+++ packages/SystemUI/src/com/android/systemui/assist/AssistManager.java
@@ -123,7 +123,8 @@ public class AssistManager {
     }
 
     public void startAssist(Bundle args) {
-        updateAssistInfo();
+        onConfigurationChanged();
+		updateAssistInfo();
         if (mAssistComponent == null) {
             return;
         }
