eb81475a9ac2e3d93e4bdb30fd13de9110abd5bb
songjinshi <songjinshi@xiaomi.com>
2017-03-15 16:16:05 +0800

    fix system_server crash issue caused by fd leak.
    If an app incorrect use of registerListener, it will cause
    system_server socket fd leak, for example:
        protected void onResume() {
            super.onResume();
            mSensorManager.registerListener(new MySensorEventListener(),
                    mSensor, SensorManager.SENSOR_DELAY_UI);
    Each time a new SensorEventQueue, then in the system_server
    will open a new socket fd, as time increases system_server's
    fd will be more than 1024 and crash, so we needed add count limit
    for sensor listener to improve the system stability.
    
    Test: use the apk attached in the issue
    
    https://code.google.com/p/android/issues/detail?id=258634
    
    Change-Id: I35006966a1638c25bb0f54611e117e16a764e12b
    Signed-off-by: songjinshi <songjinshi@xiaomi.com>

--- core/java/android/hardware/SystemSensorManager.java
+++ core/java/android/hardware/SystemSensorManager.java
@@ -16,9 +16,7 @@
 
 package android.hardware;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.MessageQueue;
@@ -40,6 +38,7 @@ import java.util.List;
  * @hide
  */
 public class SystemSensorManager extends SensorManager {
+    private static final int MAX_LISTENER_COUNT = 128;
     private static native void nativeClassInit();
     private static native long nativeCreate(String opPackageName);
     private static native boolean nativeGetSensorAtIndex(long nativeInstance,
@@ -115,6 +114,11 @@ public class SystemSensorManager extends SensorManager {
             Log.e(TAG, "maxBatchReportLatencyUs and delayUs should be non-negative");
             return false;
         }
+        if (mSensorListeners.size() >= MAX_LISTENER_COUNT) {
+            throw new IllegalStateException("register failed, " +
+                "the sensor listeners size has exceeded the maximum limit " +
+                MAX_LISTENER_COUNT);
+        }
 
         // Invariants to preserve:
         // - one Looper per SensorEventListener
@@ -174,6 +178,12 @@ public class SystemSensorManager extends SensorManager {
         if (listener == null) throw new IllegalArgumentException("listener cannot be null");
 
         if (sensor.getReportingMode() != Sensor.REPORTING_MODE_ONE_SHOT) return false;
+
+        if (mTriggerListeners.size() >= MAX_LISTENER_COUNT) {
+            throw new IllegalStateException("request failed, " +
+                    "the trigger listeners size has exceeded the maximum limit " +
+                    MAX_LISTENER_COUNT);
+        }
 
         synchronized (mTriggerListeners) {
             TriggerEventQueue queue = mTriggerListeners.get(listener);
